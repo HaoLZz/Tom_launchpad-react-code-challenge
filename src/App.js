780c662c5f4087ffbbc369f3b29b56834dc42d19
Haoliang Zhang <hlzhang26@outlook.com>
2022-06-27 17:02:19 -0600

    Add the AppBar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import ResponsiveAppBar from './app/AppBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <ResponsiveAppBar />
     </div>
   );
 }
