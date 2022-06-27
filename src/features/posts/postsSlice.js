94d59b3cbb574122f5f0a17cad458df35baa0d39
Haoliang Zhang <hlzhang26@outlook.com>
2022-06-27 17:16:50 -0600

    Add posts slice

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [
+  {
+    userId: 1,
+    id: 1,
+    title: 'First Post',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae deleniti architecto sit expedita, quia perspiciatis, delectus, excepturi tenetur dolorem accusamus at temporibus qui nesciunt quasi!',
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: 'Second Post',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae deleniti architecto sit expedita, quia perspiciatis, delectus, excepturi tenetur dolorem accusamus at temporibus qui nesciunt quasi!',
+  },
+  {
+    userId: 2,
+    id: 3,
+    title: 'Another Post',
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae deleniti architecto sit expedita, quia perspiciatis, delectus, excepturi tenetur dolorem accusamus at temporibus qui nesciunt quasi!',
+  },
+];
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {},
+});
+
+export default postsSlice.reducer;
