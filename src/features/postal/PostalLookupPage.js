a11ddd90a22d830d5dd62426642039dd7e0f6d40
Haoliang Zhang <hlzhang26@outlook.com>
2022-06-29 15:45:58 -0600

    Add PostalSearchBar component

--- src/features/postal/PostalLookupPage.js
+++ src/features/postal/PostalLookupPage.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import PostalSearchBar from './PostalSearchBar';
 
 export default function PostalLookupPage() {
+  const [searchInput, setSearchInput] = useState('');
+
   return (
     <Container>
       <Typography variant="h2" component="h2" marginY={5}>
         Postal Code Search
       </Typography>
-      <div>SearchBar</div>
-      <div>Map</div>
+      <PostalSearchBar
+        searchInput={searchInput}
+        setSearchInput={setSearchInput}
+      />
+      <Paper
+        elevation={3}
+        sx={{ paddingX: 2, paddingY: 1, marginBottom: '1.5rem' }}
+      >
+        <Box
+          marginBottom={2}
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <Typography variant="h5" component="h3" noWrap>
+            Postal Code
+          </Typography>
+        </Box>
+        <Box marginBottom={1}>
+          <Typography variant="subtitle1" component="span">
+            Coutry + country abbreviation
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant="body1" component="p" overflow="hidden">
+            "places": "place name": "Beverly Hills", "longitude": "-118.4065",
+            "state": "California", "state abbreviation": "CA", "latitude":
+            "34.0901"
+          </Typography>
+        </Box>
+      </Paper>
+      <div>Goople Map</div>
     </Container>
   );
 }
---
+++ src/features/postal/PostalSearchBar.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import { Button } from '@mui/material';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.primary.main, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.primary.main, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+      '&:focus': {
+        width: '30ch',
+      },
+    },
+  },
+}));
+
+export default function PostalSearchBar({ searchInput = '', setSearchInput }) {
+  const onSearchInput = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  return (
+    <Box
+      sx={{
+        display: {
+          xs: 'block',
+          md: 'flex',
+          justifyContent: 'center',
+          marginBottom: '35px',
+        },
+      }}
+    >
+      <Box sx={{ flex: '40% 0 1' }}>
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon color="primary" />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Please enter postal code to search"
+            inputProps={{ 'aria-label': 'search' }}
+            autoFocus
+            value={searchInput}
+            onInput={onSearchInput}
+          />
+        </Search>
+      </Box>
+      <Box
+        sx={{
+          flex: '15% 0 1',
+          textAlign: { xs: 'center' },
+          margin: { xs: '15px auto', md: '0' },
+        }}
+      >
+        <Button variant="contained">Submit</Button>
+      </Box>
+    </Box>
+  );
+}
