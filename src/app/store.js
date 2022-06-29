3cab3275e25fb19b2f49de45315a774cfd04053b
Haoliang Zhang <hlzhang26@outlook.com>
2022-06-29 09:00:28 -0600

    Add countries slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import postsReducer from '../features/posts/postsSlice';
 import universitiesReducer from '../features/universities/universitiesSlice';
+import countriesReducer from '../features/countries/countriesSlice';
 
 export const store = configureStore({
   reducer: {
     posts: postsReducer,
     universities: universitiesReducer,
+    countries: countriesReducer,
   },
 });
---
+++ src/features/countries/countriesSlice.js
@@ -0,0 +1,42 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { client } from '../../api/client';
+
+export const fetchCountries = createAsyncThunk(
+  'countries/fetchCountries',
+  async () => {
+    const response = await client.get(
+      'https://countriesnow.space/api/v0.1/countries/info?returns=none',
+    );
+    return response.data;
+  },
+);
+
+const initialState = {
+  data: [],
+  status: 'idle',
+  error: null,
+};
+
+const countriesSlice = createSlice({
+  name: 'countries',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchCountries.pending, (state, action) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchCountries.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.data = action.payload;
+      })
+      .addCase(fetchCountries.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const selectAllCountries = (state) => state.countries.data;
+
+export default countriesSlice.reducer;
