7aa43da24cd18b0a1cddc82c0ad3097b046dc07d
Haoliang Zhang <hlzhang26@outlook.com>
2022-06-30 11:51:36 -0600

    Refactor GoogleMap component Replace LoadScript with useJsApiLoader hook to avoid redundant loading of Google Map API

--- src/features/postal/GoogleMap.js
+++ src/features/postal/GoogleMap.js
@@ -1,23 +1,51 @@
-import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import React, { useState, useCallback } from 'react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 const MapContainer = ({
-  zoom = 13,
+  zoom = 10,
   center = {
     lat: 41.3851,
     lng: 2.1734,
   },
 }) => {
-  const mapStyles = {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
+  });
+
+  const containerStyle = {
     height: '50vh',
     width: '100%',
   };
 
-  return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY}>
-      <GoogleMap mapContainerStyle={mapStyles} zoom={zoom} center={center} />
-    </LoadScript>
+  const [map, setMap] = useState(null);
+
+  const onLoad = useCallback(
+    function callback(map) {
+      const bounds = new window.google.maps.LatLngBounds(center);
+      map.fitBounds(bounds);
+      setMap(map);
+    },
+    [center],
+  );
+
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={zoom}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      <> Marker</>
+    </GoogleMap>
+  ) : (
+    <></>
   );
 };
 
-export default MapContainer;
+export default React.memo(MapContainer);
